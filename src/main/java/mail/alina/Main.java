10ef24a524669b76078af2d02249b6cdd5aa6575
Alina K <alina885@mail.ru>
2025-03-03 20:06:16 +0300

    some changes

--- src/main/java/mail/alina/Main.java
+++ src/main/java/mail/alina/Main.java
@@ -2,6 +2,7 @@ package mail.alina;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World23332!");
+        System.out.println("Hello, 2222332!");
     }
 }
