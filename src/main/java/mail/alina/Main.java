fbd820c79f8199143fc37c5899b807cd3fb73930
Alina K <alina885@mail.ru>
2025-03-04 19:45:37 +0300

    fix changes

--- src/main/java/mail/alina/Main.java
+++ src/main/java/mail/alina/Main.java
@@ -3,7 +3,7 @@ package mail.alina;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, 111111!");
-        System.out.println("Hello, 222222!");
+        System.out.println("Hello, 555555!");
         System.out.println("Hello, 333333!");
     }
 }
